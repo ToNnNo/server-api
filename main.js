f3a95118c01a3bfbd674a8af26d7322e2b0ed8fd
Stéphane Menut <smenut@dawan.fr>
2021-05-27 10:12:58 +0200

    add cors

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import routes from './routes.js';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use( express.json() ); // content-type: application/json
 
 app.use(routes);
